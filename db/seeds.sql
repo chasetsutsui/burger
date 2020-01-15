8c5c112534a23180722bb18a222658f0dc66c6b1
Chase Tsutsui <chasetsutsui@yahoo.com>
2020-01-14 17:58:35 -0800

    burger app

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Hamburger');
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Cheeseburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Western Bacon Cheeseburger', true);
+
